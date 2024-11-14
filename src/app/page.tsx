a06995fcfc029c697440c4d450de8a08c05629ae
Noreen <alizak2378@gmail.com>
2024-11-14 09:11:17 -0800

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+//import styles from "./page.module.css";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+        <h1>Country List</h1>
+        <ul className="d-flex">
+          
+          <li className="cust_button"><Link className="" href="/country/pakistan">Pakistan</Link></li>
+
+          <li className="cust_button"><Link className="" href="/country/india">India</Link></li>
+
+          <li className="cust_button"><Link className="" href="/country/japan">Japan</Link></li>
+
+          <li className="cust_button"><Link className="" href="/country/saudia">Saudia</Link></li>
+
+          <li className="cust_button"><Link className="" href="/country/turkey">Turkey</Link></li>
+
+          <li className="cust_button"><Link className="" href="/country/iran">Iran</Link></li>
+
+        </ul>
+    </div>
+  );
+}
